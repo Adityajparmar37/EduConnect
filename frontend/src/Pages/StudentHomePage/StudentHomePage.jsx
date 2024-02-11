a8c047c12f0dedf18385801bf7f9f527071422bb
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-02-11 10:30:53 +0530

    student dashboard added

--- frontend/src/Pages/StudentHomePage/StudentHomePage.jsx
+++ frontend/src/Pages/StudentHomePage/StudentHomePage.jsx
@@ -1,5 +1,51 @@
 import React from "react";
+import { BsPersonVideo2 } from "react-icons/bs";
+import { IoArrowForwardSharp } from "react-icons/io5";
+import { LiaFileUploadSolid } from "react-icons/lia";
+import { TbHexagonLetterA } from "react-icons/tb";
+import { PiChatsFill } from "react-icons/pi";
+import Card from "../../Components/Card/Card";
+import SideNav from "../../Components/SideNav/SideNav";
 
 export default function StudentHomePage() {
-  return <div>StudentHomePage</div>;
+  document.body.style.overflow = "hidden";
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/6">
+        <SideNav />
+      </div>
+      <div className="z-10 flex h-auto w-5/6 px-3 pt-20">
+        <div className="grid grid-cols-3 gap-24">
+          <Card
+            link="/teacherDashboard"
+            bgColor="darkPrimary"
+            icon={<LiaFileUploadSolid />}
+            icon2={<IoArrowForwardSharp />}
+            title="Upload File"
+            text={["Upload your submission", "Upload Assignment"]}
+          />
+          <Card
+            link="/studentDashboard"
+            bgColor="darkPrimary"
+            icon={<TbHexagonLetterA />}
+            icon2={<IoArrowForwardSharp />}
+            title="Student Dashboard"
+            text={["View your attendance"]}
+          />
+
+          <Card
+            link="/subjectDashboard"
+            bgColor="darkPrimary"
+            icon={<PiChatsFill />}
+            icon2={<IoArrowForwardSharp />}
+            title="Discussion forum"
+            text={[
+              "Discuss your doubt with teacher",
+              "One to One doubt solving",
+            ]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
