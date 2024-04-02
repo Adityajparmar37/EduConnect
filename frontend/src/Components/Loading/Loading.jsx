9f8d9b156fa886248a6d3263e885247c6bc1e0c7
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-03 00:22:08 +0530

    optimaztion using lazy loading done finally 😃😃

--- frontend/src/Components/Loading/Loading.jsx
+++ frontend/src/Components/Loading/Loading.jsx
@@ -8,7 +8,9 @@ export default function Loading() {
     <div className="bg-WhiteLoading-rgba fixed left-0 top-0 z-50 h-full w-full">
       <div className="flex h-[80%] w-full flex-col items-center justify-center">
         <img src="/loading.svg" alt="Loading!" />
-        <h1 className="text-[2rem] text-darkPrimary font-semibold">Loading...</h1>
+        <h1 className="text-[2rem] font-semibold text-darkPrimary">
+          Loading...
+        </h1>
       </div>
     </div>
   );
--- frontend/src/Routes/AppRoutes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes/AppRoutes.jsx
@@ -1,34 +1,75 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import DefaultPage from "../../Pages/DefaultPage/DefaultPage";
-import Login from "../../Pages/Login/Login";
-import Signup from "../../Pages/Signup/Signup";
-import AuthRoute from "../AuthRoutes/AuthRoutes";
 import { Toaster } from "react-hot-toast";
-import AdminHomePage from "../../Pages/Admin/AdminHomePage/AdminHomePage";
-import TeacherDashboard from "../../Pages/Admin/TeacherDashboard/TeacherDashboard";
-import CreateTeacher from "../../Pages/Admin/CreateTeacher/CreateTeacher";
-import SubjectDashboard from "../../Pages/Admin/SubjectDashboard/SubjectDashboard";
-import ManageTeacher from "../../Pages/Admin/ManageTeacher/ManageTeacher";
-import ManageSubject from "../../Pages/Admin/ManageSubject/ManageSubject";
-import UpdateSubject from "../../Pages/Admin/UpdateSubject/UpdateSubject";
-import StudentDashboard from "../../Pages/Admin/StudentDashboard/StudentDashboard";
-import ManageStudent from "../../Pages/Admin/ManageStudent/ManageStudent";
-import UpdateStudent from "../../Pages/Admin/UpdateStudent/UpdateStudent";
-import StudentHomePage from "../../Pages/Student/StudentHomePage/StudentHomePage";
-import TeacherHomePage from "../../Pages/Teacher/TeacherHomePage/TeacherHomePage";
-import Attendance from "../../Pages/Teacher/Attendance/Attendance";
-import MarkSheet from "../../Pages/Teacher/Marks/MarkSheet";
-import CreateStudent from "../../Pages/Admin/CreateStudent/CreateStudent";
-import CreateSubject from "../../Pages/Admin/CreateSubject/CreateSubject";
-import UpdateTeacher from "../../Pages/Admin/UpdateTeacher/UpdateTeacher";
-import Marks from "../../Pages/Teacher/Marks/Marks";
-import AttendanceList from "../../Pages/Teacher/Attendance/AttendanceList";
-import Profile from "../../Components/Profile/Profile";
-import ViewAttendance from "../../Pages/Student/Attendance/ViewAttendance";
-import ViewMarks from "../../Pages/Student/Marks/ViewMarks";
-import DiscussionForum from "../../Pages/DiscussionForum/DiscussionForum";
 import { useAuth } from "../../Hooks/useAuth";
 import UnauthorizedMessage from "../../Components/Unauthorized/UnauthorizedMessage";
+import AuthRoute from "../AuthRoutes/AuthRoutes";
+import FallbackUI from "../../Components/FallbackUI/FallbackUI";
+
+const DefaultPage = lazy(() => import("../../Pages/DefaultPage/DefaultPage"));
+const Login = lazy(() => import("../../Pages/Login/Login"));
+const Signup = lazy(() => import("../../Pages/Signup/Signup"));
+const AdminHomePage = lazy(
+  () => import("../../Pages/Admin/AdminHomePage/AdminHomePage"),
+);
+const TeacherDashboard = lazy(
+  () => import("../../Pages/Admin/TeacherDashboard/TeacherDashboard"),
+);
+const CreateTeacher = lazy(
+  () => import("../../Pages/Admin/CreateTeacher/CreateTeacher"),
+);
+const SubjectDashboard = lazy(
+  () => import("../../Pages/Admin/SubjectDashboard/SubjectDashboard"),
+);
+const ManageTeacher = lazy(
+  () => import("../../Pages/Admin/ManageTeacher/ManageTeacher"),
+);
+const ManageSubject = lazy(
+  () => import("../../Pages/Admin/ManageSubject/ManageSubject"),
+);
+const UpdateSubject = lazy(
+  () => import("../../Pages/Admin/UpdateSubject/UpdateSubject"),
+);
+const StudentDashboard = lazy(
+  () => import("../../Pages/Admin/StudentDashboard/StudentDashboard"),
+);
+const ManageStudent = lazy(
+  () => import("../../Pages/Admin/ManageStudent/ManageStudent"),
+);
+const UpdateStudent = lazy(
+  () => import("../../Pages/Admin/UpdateStudent/UpdateStudent"),
+);
+const StudentHomePage = lazy(
+  () => import("../../Pages/Student/StudentHomePage/StudentHomePage"),
+);
+const TeacherHomePage = lazy(
+  () => import("../../Pages/Teacher/TeacherHomePage/TeacherHomePage"),
+);
+const Attendance = lazy(
+  () => import("../../Pages/Teacher/Attendance/Attendance"),
+);
+const MarkSheet = lazy(() => import("../../Pages/Teacher/Marks/MarkSheet"));
+const CreateStudent = lazy(
+  () => import("../../Pages/Admin/CreateStudent/CreateStudent"),
+);
+const CreateSubject = lazy(
+  () => import("../../Pages/Admin/CreateSubject/CreateSubject"),
+);
+const UpdateTeacher = lazy(
+  () => import("../../Pages/Admin/UpdateTeacher/UpdateTeacher"),
+);
+const Marks = lazy(() => import("../../Pages/Teacher/Marks/Marks"));
+const AttendanceList = lazy(
+  () => import("../../Pages/Teacher/Attendance/AttendanceList"),
+);
+const Profile = lazy(() => import("../../Components/Profile/Profile"));
+const ViewAttendance = lazy(
+  () => import("../../Pages/Student/Attendance/ViewAttendance"),
+);
+const ViewMarks = lazy(() => import("../../Pages/Student/Marks/ViewMarks"));
+const DiscussionForum = lazy(
+  () => import("../../Pages/DiscussionForum/DiscussionForum"),
+);
 
 export default function AppRoutes() {
   const { user } = useAuth();
@@ -36,70 +77,66 @@ export default function AppRoutes() {
   return (
     <>
       <Toaster position="top-center" toastOptions={{ duration: 2000 }} />
-      <Routes>
-        {/* header and user */}
-        <Route path="/" element={<DefaultPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+      <Suspense fallback={<FallbackUI />}>
+        <Routes>
+          <Route path="/" element={<DefaultPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
-        {/* Profile */}
-        <Route
-          path="/profile"
-          element={
-            user ? (
-              <AuthRoute>
-                <Profile />
-              </AuthRoute>
-            ) : (
-              <Navigate to="/login" replace />
-            )
-          }
-        />
+          <Route
+            path="/profile"
+            element={
+              user ? (
+                <AuthRoute>
+                  <Profile />
+                </AuthRoute>
+              ) : (
+                <Navigate to="/login" replace />
+              )
+            }
+          />
 
-        {/* admin pages */}
-        {user && user.userType === "admin" && (
-          <>
-            <Route path="/admin" element={<AdminHomePage />} />
-            <Route path="/teacherDashboard" element={<TeacherDashboard />} />
-            <Route path="/createTeacher" element={<CreateTeacher />} />
-            <Route path="/subjectDashboard" element={<SubjectDashboard />} />
-            <Route path="/createSubject" element={<CreateSubject />} />
-            <Route path="/manageTeacher" element={<ManageTeacher />} />
-            <Route path="/updateTeacher/:id" element={<UpdateTeacher />} />
-            <Route path="/manageSubject" element={<ManageSubject />} />
-            <Route path="/updateSubject/:id" element={<UpdateSubject />} />
-            <Route path="/studentDashboard" element={<StudentDashboard />} />
-            <Route path="/manageStudent/" element={<ManageStudent />} />
-            <Route path="/createStudent/" element={<CreateStudent />} />
-            <Route path="/updateStudent/:id" element={<UpdateStudent />} />
-          </>
-        )}
+          {user && user.userType === "admin" && (
+            <>
+              <Route path="/admin" element={<AdminHomePage />} />
+              <Route path="/teacherDashboard" element={<TeacherDashboard />} />
+              <Route path="/createTeacher" element={<CreateTeacher />} />
+              <Route path="/subjectDashboard" element={<SubjectDashboard />} />
+              <Route path="/createSubject" element={<CreateSubject />} />
+              <Route path="/manageTeacher" element={<ManageTeacher />} />
+              <Route path="/updateTeacher/:id" element={<UpdateTeacher />} />
+              <Route path="/manageSubject" element={<ManageSubject />} />
+              <Route path="/updateSubject/:id" element={<UpdateSubject />} />
+              <Route path="/studentDashboard" element={<StudentDashboard />} />
+              <Route path="/manageStudent/" element={<ManageStudent />} />
+              <Route path="/createStudent/" element={<CreateStudent />} />
+              <Route path="/updateStudent/:id" element={<UpdateStudent />} />
+            </>
+          )}
 
-        {/* student pages */}
-        {user && user.userType === "student" && (
-          <>
-            <Route path="/student" element={<StudentHomePage />} />
-            <Route path="/viewAttendance" element={<ViewAttendance />} />
-            <Route path="/viewMarks" element={<ViewMarks />} />
-            <Route path="/discussionForum" element={<DiscussionForum />} />
-          </>
-        )}
+          {user && user.userType === "student" && (
+            <>
+              <Route path="/student" element={<StudentHomePage />} />
+              <Route path="/viewAttendance" element={<ViewAttendance />} />
+              <Route path="/viewMarks" element={<ViewMarks />} />
+              <Route path="/discussionForum" element={<DiscussionForum />} />
+            </>
+          )}
 
-        {/* teacher pages */}
-        {user && user.userType === "teacher" && (
-          <>
-            <Route path="/teacher" element={<TeacherHomePage />} />
-            <Route path="/attendance" element={<Attendance />} />
-            <Route path="/attendance/:id" element={<AttendanceList />} />
-            <Route path="/marks" element={<Marks />} />
-            <Route path="/marks/:id" element={<MarkSheet />} />
-          </>
-        )}
+          {user && user.userType === "teacher" && (
+            <>
+              <Route path="/teacher" element={<TeacherHomePage />} />
+              <Route path="/attendance" element={<Attendance />} />
+              <Route path="/attendance/:id" element={<AttendanceList />} />
+              <Route path="/marks" element={<Marks />} />
+              <Route path="/marks/:id" element={<MarkSheet />} />
+            </>
+          )}
 
-        {/* Unauthorized access message */}
-        <Route path="*" element={<Navigate to="/unauthorized" replace />} />
-        <Route path="/unauthorized" element={<UnauthorizedMessage />} />
-      </Routes>
+          <Route path="/unauthorized" element={<UnauthorizedMessage />} />
+          <Route path="*" element={<Navigate to="/unauthorized" />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
