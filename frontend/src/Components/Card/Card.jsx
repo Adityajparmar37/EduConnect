7593ed3b5e04f7d81aa67f67fd6a467cec9a4ab5
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-04-29 11:51:30 +0530

    card bug solve

--- frontend/src/Components/Card/Card.jsx
+++ frontend/src/Components/Card/Card.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 import { GoTriangleRight } from "react-icons/go";
 
 const Card = ({ bgColor, icon, title, text, link, icon2 }) => {
+
   return (
     <>
       <Link to={link}>
         <div
-          className={`relative flex h-[5rem] w-[25rem] flex-col rounded-md border-l-8 border-${bgColor} bg-white p-5 shadow-lg duration-300 hover:shadow-xl hover:bg-blue-50 ease-in-out`}
+          className={`relative flex h-[5rem] w-[25rem] flex-col rounded-md border-l-8 ${bgColor === "darkPrimary" ? "border-darkPrimary" : "border-mintPrimary"} bg-white p-5 shadow-lg duration-300 ease-in-out hover:bg-blue-50 hover:shadow-xl`}
           style={{ zIndex: 1 }}
         >
           <div>
