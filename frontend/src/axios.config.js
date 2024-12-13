8f9c9c268810ed13b64affff11fdaac3eacaceb2
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-12-13 19:37:14 +0530

    minor changes

--- frontend/src/axios.config.js
+++ frontend/src/axios.config.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.MODE !== "development"
-    ? "http://localhost:8080" // Backend URL for local development
-    : "http://educonnect-backend.ap-southeast-1.elasticbeanstalk.com"; // Production backend URL
-
-axios.defaults.baseURL = baseURL;
-
-console.log(`Base URL: ${axios.defaults.baseURL}`);
+axios.defaults.baseURL =
+  // eslint-disable-next-line no-undef
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3500"
+    : "http://educonnect-backend.ap-southeast-1.elasticbeanstalk.com/";
