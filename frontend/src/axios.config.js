796ce24d17a4fbca47bfd008b35d2c90ab02fc44
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2024-12-14 11:08:49 +0530

    backend url added

--- frontend/src/axios.config.js
+++ frontend/src/axios.config.js
@@ -1,5 +1,3 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV !== "production" ? "http://localhost:3500" : "/";
+axios.defaults.baseURL = "http://educonnect-backend.ap-southeast-1.elasticbeanstalk.com/"
